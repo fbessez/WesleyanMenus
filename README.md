2df936d1644258ee0ec04de3fb510a0d993384fa
Fabien Bessez <fabien.bessez@gmail.com>
2017-04-09 10:22:10 -0400

    Attributes working! BonAppetit near completion

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Compiling Wesleyan Food Service Menus into one location for optimal decision making capabilities!
 
 Will have daily menus rendered for: 
-Weswings
-Summies
-Usdan
-Star and Crescent
-Red & Black
+Weswings --> not always correct menu
+Summies --> are specials ever posted?
+Usdan --> Just need the items constant
+Star and Crescent --> Got it!
+Red & Black --> Look here next
--- menu_fetching.ex
+++ menu_fetching.ex
@@ -1,16 +1,21 @@
 # mix run menu_fetching.ex
 
+defmodule Common do
+	def get_body(url) do
+		HTTPoison.get!(url).body
+		|> Floki.parse
+		|> Floki.find(".entry-title a")
+		|> Floki.filter_out("i")
+	end
+end
 
 
 ### Star & Crescent
 ### I could just grab the whole week at once
 defmodule StarandCrescent.Menu do
-	# Need to either scrape for the correct link or just guess it...
 	def get_body do
 		url = "http://wesleying.org/author/star-and-crescent/"
-		HTTPoison.get!(url).body
-		|> Floki.parse
-		|> Floki.find(".entry-title a")
+		Common.get_body(url)
 		|> find_correct_post
 		|> scrape_menu_items
 		# |> Floki.find(".entry-content")
@@ -18,33 +23,30 @@ defmodule StarandCrescent.Menu do
 	end
 
 	def find_correct_post(found_floki) when length(found_floki) == 0, do: false
-	# IT is not getting the right head --> too many options to choose from
 	def find_correct_post(found_floki) do
-		curr = hd found_floki
-# 		IO.inspect curr -> 
-# 			{"a",
-#  [{"href",
-#    "http://wesleying.org/2017/04/05/star-and-crescent-closed-wednesday-45/"},
-#   {"title", "Permalink to Star and Crescent CLOSED Wednesday, 4/5"},
-#   {"rel", "bookmark"}], ["Star and Crescent CLOSED Wednesday, 4/5"]}
-# {"a", [{"href", ""}], [{"i", [{"class", ""}], []}]}
-		title = hd(elem(curr, 2))
-		IO.inspect title
-		case String.slice(title, 0, 22) do
+		case (hd found_floki) do
+			{"a", [{"href", ""}], [{"i", [{"class", ""}], []}]} -> find_correct_post((tl found_floki))
+			{"a", [{"href", ""}], []} -> find_correct_post((tl found_floki))
+			_ -> 
+				curr = hd found_floki
+				title = hd(elem(curr, 2))
+				case String.slice(title, 0, 22) do
 		# case title = "Star and Crescent Menu"
 		# this should be grabbing top down so most recent
 		# otherwise i can match with the current week 4/4 or 3/27 or whatever
-			"Star and Crescent Menu" -> 
-				Floki.attribute(curr, "href")
-				|> hd()
-			_ -> find_correct_post(tl found_floki)
+					"Star and Crescent Menu" -> 
+						Floki.attribute(curr, "href")
+						|> hd()
+					_ -> find_correct_post(tl found_floki)
+				end
 		end
 	end
 
 	def scrape_menu_items(false), do: "Sorry couldn't find the menu. Check out \"http://wesleying.org/author/star-and-crescent/\""
 	def scrape_menu_items(url) do
-		IO.puts "Not Yet Implemented"
-		{ok, res} = HTTPoison.get(url, [])
+		IO.puts url
+		# IO.puts "Not Yet Implemented"
+		{:ok, res} = HTTPoison.get(url, [])
 		res.body
 		|> Floki.parse()
 		|> Floki.find("div, .entry-content")
@@ -53,7 +55,7 @@ defmodule StarandCrescent.Menu do
 	end
 end
 
-# StarandCrescent.Menu.get_body()
+# IO.inspect StarandCrescent.Menu.get_body()
 
 
 ### WesWings --> COMPLETE-ish
@@ -61,10 +63,7 @@ end
 defmodule Weswings.Menu do
 
 	def get_body() do
-		url = "https://weswings.com/category/weswings-specials/"
-		HTTPoison.get!(url).body
-		|> Floki.parse
-		|> Floki.find(".entry-title a")
+		Common.get_body("https://weswings.com/category/weswings-specials/")
 		|> find_correct_post
 		|> scrape_menu_items
 	end
@@ -104,26 +103,12 @@ end
 # IO.inspect Weswings.Menu.get_body()
 
 
-defmodule BonAppetit.Menu do
-	# def cafe do
-	# 	"332"
-	# end
+
+defmodule BonAppetit.Fetch do
 	@usdan "332"
 	@summies "337"
 	@usdanurl "http://legacy.cafebonappetit.com/api/2/menus?cafe=#{@usdan}"
 	@summiesurl "http://legacy.cafebonappetit.com/api/2/menus?cafe=#{@summies}"
-
-	# def url do
-	# 	cafe = cafe()
-	# 	'http://legacy.cafebonappetit.com/api/2/menus?cafe=#{cafe}'
-	# end
-
-	# def item_list do
-	# 	body_map()["items"]
-	# end
-
-	# http://elixir-lang.org/getting-started/module-attributes.html#as-constants
-
 
 	def fetch_usdan() do
 		{:ok, response} = HTTPoison.get(@usdanurl, [])
@@ -134,29 +119,25 @@ defmodule BonAppetit.Menu do
 		{:ok, response} = HTTPoison.get(@summiesurl, [])
 		secondary(response)
 	end
+
 	def secondary(response) do
 	    response
 	    |> Map.from_struct
 	    |> Map.fetch!(:body)
-	    |> decode_body
+	    |> Poison.decode!
 	end
+end
 
-	def decode_body(body) do
-		{:ok, body_map} = Poison.decode(body)
-		body_map
-	end
+defmodule Usdan.Menu do
+	@usdan "332"
+	@summies "337"
+	@body_map BonAppetit.Fetch.fetch_usdan()
 
+	def get_body_map, do: @body_map
+	def get_items_index, do: @body_map["items"]
 
-	# http://stackoverflow.com/questions/31862889/elixir-modifying-value-of-module-attribute
-	# def body_map do
-	# 	Agent.start_link(fetch2(), name: __BODYMAP__)
-	# end
-
-	# @body_map fetch2
-	# @items_index ""
-
-	def get_all_necessary_data(body_map) do
-		menu = hd (hd body_map["days"])["cafes"][cafe]["dayparts"]
+	def get_all_necessary_data() do
+		menu = hd (hd get_body_map["days"])["cafes"][@usdan]["dayparts"]
 		IO.inspect dayparts(menu, %{})
 	end
 
@@ -185,12 +166,12 @@ defmodule BonAppetit.Menu do
 	def get_item_names(item_list, item_names) do
 		curr_item_no = hd item_list
 		item_name = get_item_name(curr_item_no)
-		item_names ++ [item_name]
+		item_names = item_names ++ [item_name]
 		get_item_names((tl item_list),item_names)
 	end
 
 	def get_item_name(item_no) do
-		item_name = @items_index[item_no]["label"]
+		get_items_index[item_no]["label"]
 	end
 
 
@@ -198,7 +179,7 @@ defmodule BonAppetit.Menu do
 # by calling the api?
 end
 
-IO.inspect BonAppetit.Menu.fetch2()
+Usdan.Menu.get_all_necessary_data()
 
 
 
